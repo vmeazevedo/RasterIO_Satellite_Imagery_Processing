93bbf1c089dd62ea0734bbaa807c6a18fba8ee4f
Vinícius Azevedo <vmeazevedo@gmail.com>
2021-07-24 16:14:12 -0300

    Update 0_landsat_aws.py
    update

--- 0_landsat_aws.py
+++ 0_landsat_aws.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 # Arquivos de satélite na nuvem
 filepath = 'http://landsat-pds.s3.amazonaws.com/c1/L8/042/034/LC08_L1TP_042034_20170616_20170629_01_T1/LC08_L1TP_042034_20170616_20170629_01_T1_B4.TIF'
 
-print('Landsat on AWS:')
+print('Landsat na AWS:')
 with rasterio.open(filepath) as src:
     print(src.profile)
 
@@ -18,16 +18,16 @@ with rasterio.open(filepath) as src:
 with rasterio.open(filepath) as src:
    oviews = src.overviews(1) # lista de visões gerais do maior ao menor
    oview = oviews[-1] # vamos olhar para a menor miniatura
-   print('\nDecimation factor= {}'.format(oview))
+   print('\nFator de dizimação = {}'.format(oview))
 
    thumbnail = src.read(1, out_shape=(1, int(src.height // oview), int(src.width // oview)))
 
-print('\narray type: ',type(thumbnail))
+print('\nTipo array: ',type(thumbnail))
 print(thumbnail)
 
 plt.imshow(thumbnail)
 plt.colorbar()
-plt.title('Overview - Band 4 {}'.format(thumbnail.shape))
+plt.title('Overview - Banda 4 {}'.format(thumbnail.shape))
 plt.xlabel('Coluna #')
 plt.ylabel('Linha #')
 plt.show()
